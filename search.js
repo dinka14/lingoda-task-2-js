e1e27bb9f7e391a6947323ace99b716ee306b715
Denis Karpelevich <dkarpelevich@odin.com>
2018-04-06 18:49:42 +0300

    initial commit

--- search.js
+++ search.js
@@ -6,11 +6,22 @@ var browser = new webdriver.Builder()
     .usingServer('http://localhost:4444/wd/hub')
     .build();
 
-function logTitle() {
+function page1() {
     browser.getTitle().then((title) => {
         console.log('Current Page Title: ' + title);
         assert.equal(title.toLowerCase().indexOf("amazon.de"), 0, "Title isn't equal to amazon.de");
     });
+    /*
+    var elem = browser.findElement(webdriver.By.id('twotabsearchtextbox'));
+    elem.sendKeys('Batman comics');
+    elem.sendKeys(webdriver.Key.RETURN);
+    */
+}
+
+function page2() {
+    var elem = browser.findElement(webdriver.By.id('twotabsearchtextbox'));
+    elem.sendKeys('Batman comics');
+    elem.sendKeys(webdriver.Key.RETURN);
 }
 
 function handleFailure(err) {
@@ -18,16 +29,18 @@ function handleFailure(err) {
     closeBrowser();
 }
 
-function checkPageOpen() {
+
+
+function checkPage1Open() {
     return browser.findElements(webdriver.By.css('[href="/ref=footer_logo"]')).then((result) => {
         return result[0];
     });
 }
 
 function closeBrowser() {
-    browser.quit();
+    browser.close();
 }
 
 browser.get('https://www.amazon.de/');
 console.log('open browser');
-browser.wait(checkPageOpen, 5000).then(logTitle).then(closeBrowser, handleFailure);
+browser.wait(checkPage1Open, 9000).then(page1).then(page2);//.then(closeBrowser, handleFailure);
