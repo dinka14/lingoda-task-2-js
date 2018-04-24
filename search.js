2ab1fdb8851056eebef631e3c5c5d3db9edcf832
denis <dinka14@yandex.ru>
2018-04-24 19:03:37 +0300

    add page2

--- search.js
+++ search.js
@@ -1,6 +1,5 @@
 "use strict";
 
-require('./waitReady.js');
 var assert = require('assert');
 var webdriver = require('selenium-webdriver');
 const until = require("selenium-webdriver/lib/until");
@@ -13,15 +12,51 @@ var browser = new webdriver.Builder()
 var find_search_field = "twotabsearchtextbox";
 var search_string = "Batman comics\n";
 var find_first_item = "//ul[@id='s-results-list-atf']//li[@id='result_0']";
+var find_title = "//ul[@id='s-results-list-atf']//li[@id='result_0']//a[@class='a-link-normal s-access-detail-page  s-color-twister-title-link a-text-normal']";
+
 
 function run() {
     browser.get('http://www.amazon.de/').then(() => {
         console.log('open browser');
         browser.wait(until.titleContains('Amazon.de'), 10000);
+
+        console.log('Try to find ' + search_string);
         var query = browser.wait(until.elementLocated(By.By.id(find_search_field)), 10000);
         query.sendKeys(search_string);
-        console.log('Try to find' + search_string);
-        browser.wait(until.elementLocated(By.By.xpath(find_first_item)), 10000);
+
+        browser.wait(until.elementLocated(By.By.xpath(find_first_item)), 10000).then(() => {
+            console.log('Check that results number above 0');
+        });
+
+        var title = browser.wait(until.elementLocated(By.By.xpath(find_title)), 10000);
+        title.then(elm => {
+            console.log('Check that title contains "Batman" word');
+            return elm.getText().then(text => {
+                console.log(text);
+                assert.ok(text.includes('Batman'), 'Title doesn\'t contain "Batman" word');
+            });
+        });
+
+
+        /*
+        browser.findElements(webdriver.By.xpath(find_title)).then((elem) => {
+            console.log('Try to find batman title ' + elem);
+            var a = elem[0].text;
+            if (a.indexOf('Batman') !== -1) {
+                return 1
+            }
+        });
+        */
+
+        /*
+        var title = browser.wait(until.elementLocated(By.By.xpath(find_title)), 10000);
+        console.log('title ' + title);
+        var a = title.text;
+        console.log(typeof a);
+        //assert.equal(a.toLowerCase().includes('batman'),);
+        //assert.notStrictEqual(title.toLowerCase(), 'batman');
+
+        */
         browser.close();
     });
 }
